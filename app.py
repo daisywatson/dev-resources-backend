0185c9cebfcc93b79456da3405a9740bb2712194
Daisy Watson <Daisy@Daisys-Air.home>
2020-11-23 19:45:01 -0500

    cors fix

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ app.register_blueprint(users, url_prefix='/api/v1/users')
 # CORS(app, supports_credentials = True)
 CORS(resources, origins=['http://localhost:3000', 'https://developer-resources-react.herokuapp.com/', supports_credentials = True]
 CORS(users, origins=['http://localhost:3000', 'https://developer-resources-react.herokuapp.com/', supports_credentials = True]
+CORS(resources, origins=['http://localhost:3000', 'https://developer-resources.herokuapp.com/', supports_credentials = True]
+CORS(users, origins=['http://localhost:3000', 'https://developer-resources.herokuapp.com/', supports_credentials = True]
+
 
 @login_manager.user_loader
 def load_user(user_id):
