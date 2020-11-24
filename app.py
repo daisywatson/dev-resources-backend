866ccbd7cfa7cf45cc4822d6437f279b5a84435a
Daisy Watson <Daisy@Daisys-Air.home>
2020-11-24 09:53:00 -0500

    flask fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
-app.config['CORS_HEADERS'] = 'Content-Type'
+# app.config['CORS_HEADERS'] = 'Content-Type'
 # @cross_origin()
 app.secret_key = "This is the secret key. Here it is."
 
